31aff7eaf1060889b97c5524aab63b34cd078bff
Jatin Sagar <jatin.sagar8770@gmail.com>
2024-12-29 21:22:07 +0530

    final testing with time ago

---
+++ resources/js/Components/TimeAgo.vue
@@ -0,0 +1,20 @@
+<template>
+    <span>{{convertedDateTime}}</span>
+    </template>
+    <script>
+    import moment from 'moment-timezone';
+    moment.tz.setDefault('Asia/Kolkata');
+    export default {
+      name: "TimeAgo",
+      props:{
+        dateTime:{
+          required:true,
+        }
+      },
+      computed:{
+        convertedDateTime(){
+         return moment(this.dateTime).local().fromNow();
+        }
+      }
+    }
+    </script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,6 +2,7 @@
 import Hero from '@/Components/Dashboard/Hero.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import TimeAgo from '@/Components/TimeAgo.vue';
 defineProps({
     jobs: Object
 });
@@ -21,11 +22,11 @@ defineProps({
                 <!-- TODO: Add job list here -->
 
                 <div class="grid grid-flow-col gap-3">
-                    <div v-for="(job, index) in jobs"
+                <div v-for="(job, index) in jobs"
                     :key="index" class="bg-white-100 col-span-4">
 
                     <div class="m-5">
-                <div class="group mx-2 mt-10 grid max-w-screen-md grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-8 text-gray-700 shadow transition hover:shadow-lg sm:mx-auto">
+                    <div class="group mx-2 mt-10 grid max-w-screen-md grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-8 text-gray-700 shadow transition hover:shadow-lg sm:mx-auto">
                     <a href="#" class="order-2 col-span-1 mt-4 -ml-14 text-left text-gray-600 hover:text-gray-700 sm:-order-1 sm:ml-4">
                     <div class="group relative h-16 w-16 overflow-hidden rounded-lg">
                         <img :src="'storage/'+job.logo" alt="" class="h-full w-full object-cover text-gray-700" />
@@ -40,6 +41,7 @@ defineProps({
                     <h3 class="text-sm text-gray-600 mb-2">{{ job.company_name }} Technologies Pvt. Ltd.</h3>
                     <p class="overflow-hidden pr-7 text-sm whitespace-nowrap">{{ job.description && job.description.length < 57 ? job.description : job.description.substring(0,57)+" ..."  }}</p>
                     </div>
+                    <div class="col-span-9"></div><div class="col-span-3 mt-2"><span class="text-sm text-gray-600 mb-2"><time-ago :dateTime='job.created_at'/></span></div>
                 </div>
                 </div>
 
